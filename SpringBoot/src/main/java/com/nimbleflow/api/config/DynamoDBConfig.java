43fcd55fd7d37568cdc705d1e482609b195daf51
Gabriel Rabelo Machado Santos <gabrielramasa@gmail.com>
2023-04-04 01:06:39 -0300

    Adding Swagger and upgrading DynamoDB version

--- SpringBoot/src/main/java/com/nimbleflow/api/config/DynamoDBConfig.java
+++ SpringBoot/src/main/java/com/nimbleflow/api/config/DynamoDBConfig.java
@@ -10,7 +10,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
 @Configuration
