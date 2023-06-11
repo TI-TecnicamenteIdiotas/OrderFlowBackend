d17cec9706fb4bdacd8ae9fd332caedb96c00f1e
Gabriel Rabelo Machado Santos <gabrielramasa@gmail.com>
2023-06-11 19:36:25 -0300

    - Removing security.

--- SpringBoot/src/main/java/com/nimbleflow/api/config/OpenAPIConfig.java
+++ SpringBoot/src/main/java/com/nimbleflow/api/config/OpenAPIConfig.java
@@ -1,7 +1,5 @@
 package com.nimbleflow.api.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,12 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-    name = "Bearer Authorization",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
-)
 public class OpenAPIConfig {
     
     @Bean
